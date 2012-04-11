b8a1b7551f8d030c5be7c6cc33991981a8c14c0c
ZiPeng Zhang <zhangzpp@mail.ustc.edu.cn>
2012-04-11 15:20:46 +0800

    fix the problem in switch.bat about "not find the file"

--- switch.bat
+++ switch.bat
@@ -1,5 +1,5 @@
-@if "%1" == "4" copy hosts.ipv4 C:\Windows\System32\drivers\etc\hosts
+@if "%1" == "4" copy %~dp0\hosts.ipv4 C:\Windows\System32\drivers\etc\hosts
 
-@if "%1" == "6" copy hosts.ipv6 C:\Windows\System32\drivers\etc\hosts
+@if "%1" == "6" copy %~dp0\hosts.ipv6 C:\Windows\System32\drivers\etc\hosts
 
-@if "%1" == "back" copy hosts.back C:\Windows\System32\drivers\etc\hosts
+@if "%1" == "back" copy %~dp0\hosts.back C:\Windows\System32\drivers\etc\hosts
